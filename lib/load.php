0ca2411da104df029f3d98aeb97478d63b906200
ramon <ramonjd@gmail.com>
2024-05-10 17:10:23 +1000

    This ensures that any theme exports get the benefit of the latest changes to theme json and resolver.

--- lib/load.php
+++ lib/load.php
@@ -47,6 +47,7 @@ if ( class_exists( 'WP_REST_Controller' ) ) {
 	require_once __DIR__ . '/compat/wordpress-6.5/rest-api.php';
 
 	// WordPress 6.6 compat.
+	require_once __DIR__ . '/compat/wordpress-6.6/class-gutenberg-rest-edit-site-export-controller-6-6.php';
 	require __DIR__ . '/compat/wordpress-6.6/class-gutenberg-rest-templates-controller-6-6.php';
 	require __DIR__ . '/compat/wordpress-6.6/rest-api.php';
 
