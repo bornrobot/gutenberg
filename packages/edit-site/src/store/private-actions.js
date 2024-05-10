445ce2d4c06bcbd56b11fe903b8e28587b946cd4
Riad Benguella <benguella@gmail.com>
2024-05-10 20:19:07 +0100

    Animate the hub using view transitions

--- packages/edit-site/src/store/private-actions.js
+++ packages/edit-site/src/store/private-actions.js
@@ -13,31 +13,43 @@ import { store as editorStore } from '@wordpress/editor';
 export const setCanvasMode =
 	( mode ) =>
 	( { registry, dispatch } ) => {
-		const isMediumOrBigger =
-			window.matchMedia( '(min-width: 782px)' ).matches;
-		registry.dispatch( blockEditorStore ).__unstableSetEditorMode( 'edit' );
-		dispatch( {
-			type: 'SET_CANVAS_MODE',
-			mode,
-		} );
-		// Check if the block list view should be open by default.
-		// If `distractionFree` mode is enabled, the block list view should not be open.
-		// This behavior is disabled for small viewports.
-		if (
-			isMediumOrBigger &&
-			mode === 'edit' &&
+		const switchCanvasMode = () => {
+			const isMediumOrBigger =
+				window.matchMedia( '(min-width: 782px)' ).matches;
 			registry
-				.select( preferencesStore )
-				.get( 'core', 'showListViewByDefault' ) &&
-			! registry
-				.select( preferencesStore )
-				.get( 'core', 'distractionFree' )
-		) {
-			registry.dispatch( editorStore ).setIsListViewOpened( true );
+				.dispatch( blockEditorStore )
+				.__unstableSetEditorMode( 'edit' );
+			dispatch( {
+				type: 'SET_CANVAS_MODE',
+				mode,
+			} );
+			// Check if the block list view should be open by default.
+			// If `distractionFree` mode is enabled, the block list view should not be open.
+			// This behavior is disabled for small viewports.
+			if (
+				isMediumOrBigger &&
+				mode === 'edit' &&
+				registry
+					.select( preferencesStore )
+					.get( 'core', 'showListViewByDefault' ) &&
+				! registry
+					.select( preferencesStore )
+					.get( 'core', 'distractionFree' )
+			) {
+				registry.dispatch( editorStore ).setIsListViewOpened( true );
+			} else {
+				registry.dispatch( editorStore ).setIsListViewOpened( false );
+			}
+			registry.dispatch( editorStore ).setIsInserterOpened( false );
+		};
+
+		if ( ! document.startViewTransition ) {
+			switchCanvasMode();
 		} else {
-			registry.dispatch( editorStore ).setIsListViewOpened( false );
+			document.startViewTransition( () => {
+				switchCanvasMode();
+			} );
 		}
-		registry.dispatch( editorStore ).setIsInserterOpened( false );
 	};
 
 /**
